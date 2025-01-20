ac74dbd6cd4b158ebe54fbf0dd2a78d56ed8a7a7
arinrahman <rahman.arin29@gmail.com>
2025-01-19 18:26:02 -0700

    highest score

---
+++ task_2.py
@@ -0,0 +1,18 @@
+num_list= [95,65,35,98,101]
+max_val= max(num_list)
+sum_val=sum(num_list)
+print(max_val)
+print(sum_val)
+
+max=0
+print(max)
+for n in num_list:
+    if n>max:
+        max=n
+print(max)
+s=0
+print(s)
+for n in num_list:
+    s+=n
+print(s)
+
